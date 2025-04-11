e76373151c528d60c6c96b9ca71a3ae5ca808e05
NU1LSPAXE\nu1ls <82641653+nu1lspaxe@users.noreply.github.com>
2025-04-12 02:26:29 +0800

    feat: thread, mutex

---
+++ thread-basic/src/main.rs
@@ -0,0 +1,81 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle2.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    let (tx2, rx2) = mpsc::channel();
+    let tx2_clone = tx2.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        
+        for val in vals {
+            tx2_clone.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx2 {
+        println!("Got from clone: {}", received);
+    }
+}
